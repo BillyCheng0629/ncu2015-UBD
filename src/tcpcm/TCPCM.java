d60c0d818b53e65ec241e6bb454fb8e086f51df0
Lawrence <jinnim56395@gmail.com>
2015-12-27 20:40:04 +0800

    first modify TCPSM TCPCM

--- src/tcpcm/TCPCM.java
+++ src/tcpcm/TCPCM.java
@@ -1,5 +1,110 @@
 package tcpcm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+import javax.print.event.PrintJobAdapter;
+import javax.security.auth.SubjectDomainCombiner;
+
+import dom.DOM;
+
 public class TCPCM {
+	private int clientNo;
+	private String serverIP;
+	private int serverPort = 8888;
+	private Socket socket;
+	private Thread recieveThread;
+	private BufferedReader in;
+	private DOM dom;
 	
+	
+
+	
+	
+	/*
+	public TCPClient(InetAddress serverIP, int serverPort) {
+		this.serverIP =serverIP;
+		this.serverPort = serverPort;
+		
+		
+	}
+	*/
+	
+	
+	public Boolean connectServer(String ip, int port) {
+		serverIP = ip;
+		serverPort = port;
+		
+
+		try {
+			socket = new Socket(ip, port);
+			return true;
+		} catch (IOException e) {
+			System.out.println("Error:"+e.getMessage());
+			return false;
+		}
+		
+	}
+	
+	public void inputAction(int actionCode) throws IOException {
+		PrintStream out = new PrintStream(socket.getOutputStream());
+		
+		assert (actionCode >= 0 && actionCode<=5);
+		switch (actionCode) {
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+			
+			out.println("MOVE,"+actionCode);
+			out.flush();
+			
+			break;
+		case 5:
+			out.println("PLACE,");
+			out.flush();
+
+		default:
+			break;
+		}
+		
+		
+	}
+	
+	
+	public void sendRoomAction(String msg) throws IOException {
+		PrintStream out = new PrintStream(socket.getOutputStream());
+		out.println(msg);
+		out.flush();
+	}
+	
+	public void recieveMessage() {
+		recieveThread = new Thread() {
+			public void run()
+	        {
+				while(true) {
+					try {
+						in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+						String msg = in.readLine();
+						
+						
+						
+						
+						
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+	        }
+		};
+	}
+	
+	public void setDOM(DOM dom) {
+		this.dom = dom;
+	}
 }
--- src/tcpsm/TCPSM.java
+++ src/tcpsm/TCPSM.java
@@ -1,5 +1,221 @@
 package tcpsm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+
+import cdc.CDC;
+import gameobject.Player;
+
 public class TCPSM {
+	private int listenPort =8001; //default port
+	private ServerSocket serverSocket;
+
+	private Socket clientSocket; // the socket
+	private Vector<Socket> clientSocketTable;
+
+	private Vector<String> clientIPTable;
+	private Vector connectiontable;
+	private int count;
+	private Thread t;
+	private int clientLimit = 4;
+	private CDC cdc;
+	private PrintStream out = null;
 	
+	
+	public TCPSM(int listenPort) {
+		assert (listenPort>0 && listenPort<=65535);
+		this.listenPort = listenPort;
+		
+	}
+	
+	
+
+	public void initTCPserver() {
+		t = new Thread()
+	    {
+	        public void run()
+	        {
+	        	
+					
+	    		clientIPTable = new Vector<String>();
+	    		connectiontable = new Vector();
+	    		count = 0;
+	    		
+	    		try {
+	    			serverSocket = new ServerSocket(listenPort);
+	    		} catch (IOException e1) {
+	    			// TODO Auto-generated catch block
+	    			System.out.println("server socket error ");
+	    			e1.printStackTrace();
+	    		}
+	    		
+
+	    		//System.out.println("the server information:" + serverSocket.getLocalSocketAddress());
+	    		Thread thread = null;
+	    		while (count<clientLimit && !serverSocket.isClosed()) {
+	    			try {
+	    				//System.out.println("wait to client....");
+	    				while(true) {
+	    					if (count<clientLimit)
+	    						break;
+	    				}
+	    				clientSocket = serverSocket.accept();
+	    				
+	    				clientSocketTable.add(clientSocket);
+	    				ServerThread now;
+	    				
+	    				thread = new Thread(now = new ServerThread(count++, clientSocket, cdc, clientSocketTable) );
+	    				connectiontable.addElement(now);
+	    				clientIPTable.add(clientSocket.getRemoteSocketAddress().toString().split("/")[1].split(":")[0]);
+	    				thread.start();
+
+	    			} catch (IOException e) {
+	    				System.out.println("some error:" + e.getMessage());
+	    			}
+
+	    		}
+	            
+	        }
+	        
+	    };
+	    t.start();
+		
+		
+
+	}
+	
+	public void setCDC(CDC cdc) {
+		this.cdc = cdc;
+	}
+	
+	public Vector<String> getClientIPTable() {
+		return clientIPTable;
+	}
+	
+	public boolean isBound() {
+		
+		return serverSocket.isBound();
+	}
+	
+	public int getListenPort() {
+		return listenPort;
+	}
+	
+	
+		
+	
+	
+	public void closeTCP() throws IOException {
+		//serverSocket.setReuseAddress(true);
+		serverSocket.close();
+		t.interrupt();
+		
+	}
+}
+
+
+class ServerThread implements Runnable {
+
+	Socket clientSocket = null;
+	PrintStream out = null;
+	BufferedReader in = null;
+	int clientNo;
+	CDC cdc;
+	
+	Vector<Socket> clientSocketTable;
+	
+	// config
+	
+
+
+	public ServerThread(int clientNo, Socket clientSocket, CDC cdc, Vector<Socket> clientSocketTable) {
+		// TODO Auto-generated constructor stub
+		this.clientNo = clientNo;
+		this.clientSocket = clientSocket;
+		this.cdc = cdc ;
+		this.clientSocketTable = clientSocketTable;
+
+	}
+	
+	public void  broadcast(String msg) throws IOException {
+		for (int i=0;i<clientSocketTable.size();i++) {
+			out = new PrintStream(clientSocketTable.get(i).getOutputStream());
+			out.println(msg);
+			out.flush();
+		}
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		//System.out.println("the client information :"+ (clientSocket.getRemoteSocketAddress()+"").split("/")[1].split(":")[0]);
+		Player player = new Player("");
+		cdc.addPlayer(player, clientNo);
+		assert clientSocket.isConnected();
+		while (clientSocket.isConnected()) {
+			try {
+				in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+				String msg = in.readLine();
+				
+				String action = msg.split(",")[0];
+				
+				
+				
+				
+				
+				
+				
+				
+				switch (action) {
+					case "MOVE":
+						int moveCode = Integer.parseInt(msg.split(",")[1]);
+						
+						//System.out.println("move code: "+ moveCode);
+						cdc.updateDirection(clientNo, moveCode);
+						break;
+					case "PLACE":
+						//cdc.getItem(clientNo);
+						break;
+						
+					case "SETNAME":
+						String name = msg.split(",")[1];
+						cdc.getPlayer(clientNo).setName(name);
+						break;
+						
+					case "SETMAP":
+						
+						broadcast(msg);
+						break;
+						
+					case "SETCHARACTER":
+						
+						//cdc.getPlayer(clientNo).getCharacter().setCharacterImg(characterImg);;
+						broadcast("SETCHARACTER,");
+						break;
+	
+					default:
+						
+						break;
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	
+
+
+
 }
