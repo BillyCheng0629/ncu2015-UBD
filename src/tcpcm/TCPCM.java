ac2dab2e235af5c28f3df5695040fb4d1a5710f3
kevin <k8101026666@gmail.com>
2015-12-27 15:14:25 +0800

    add tcp file

--- src/tcpcm/TCPCM.java
+++ src/tcpcm/TCPCM.java
@@ -1,5 +1,50 @@
 package tcpcm;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+
 public class TCPCM {
+	Socket client; 
+	final int port = 1234;
 	
+	TCPCM(){
+
+	}
+	
+	boolean connectServer(InetAddress serverip){
+		try {
+			client=new Socket(serverip.getHostAddress(), port);
+		} catch (SocketTimeoutException e) {
+	        client=null;
+			e.printStackTrace();
+			return false;
+	    } catch (UnknownHostException e) {
+			client=null;
+			e.printStackTrace();
+			return false;
+		} catch (IOException e) {
+			client=null;
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
+	void inputMoves(int MoveCode){
+		switch (MoveCode) {
+		case 1:
+			break;
+		}
+	}
+	
+	void sendRoomAction(String msg){
+		
+	}
+	
+	void recieveMessage(String msg){
+		
+	}
 }
--- src/tcpsm/TCPSM.java
+++ src/tcpsm/TCPSM.java
@@ -1,5 +1,57 @@
 package tcpsm;
 
+import java.io.IOException;
+import java.util.Vector;
+
+import cdc.CDC;
+
 public class TCPSM {
+	Thread listenTread;
+	TCPSM() {}
 	
+	/**
+	 * called by UIM to start TCP server
+	 * and begin to listen connections from client computers.
+	 * The server should maintain a table to keep the ip addresss
+	 * of client computers which connects to this server. 
+	 */
+	void initTCPServer() throws IOException{
+		listenTread = new Thread(Acceptor.getInstance());
+		listenTread.start();
+	}
+	
+	/**
+	 * called by UDPBC
+	 * After all the connections are established
+	 * UDPBC may starts to broadcast.
+	 * Before it starts to broadcast, it will need the IP addresses
+	 * of the client computer
+	 * this method return all the IP addresses of client computer
+	 * and put them in a vector
+	 * 
+	 * @return      IP table(String)
+	 */
+	Vector<String> getClientIPTable() {
+		assert listenTread!= null: "getClientIPTable() :need to init TCP server first";
+		return Acceptor.getInstance().getIPTable();
+	}
+	
+	/**
+	 * call by UIM
+	 * set a new data center to TCP server
+	 * 
+	 * @param  cdc  xxx
+	 */
+	void setCDC(CDC cdc){
+		
+	}
+	
+	/** 
+	 * send message to all client
+	 * 
+	 * @param message the string to send
+	 */
+	void broadcast(String message) {
+		
+	}
 }
